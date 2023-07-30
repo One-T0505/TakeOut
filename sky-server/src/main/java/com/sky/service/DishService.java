9da0a5d2c8a77761f26cc474db6d317735f0cfb0
oneT <15549316580@163.com>
2023-07-30 15:30:15 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * ymy
@@ -12,7 +14,13 @@ public interface DishService {
     /*
     * 新增菜品和对应的风味
     * @param dishDTO
-    *
     * */
     void saveWithFlavor(DishDTO dishDTO);
+
+
+    /*
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
