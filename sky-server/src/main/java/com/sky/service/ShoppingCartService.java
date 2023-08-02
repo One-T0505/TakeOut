2a07708eae565b9c15b24bb8c15adbc3c39555d0
oneT <15549316580@163.com>
2023-08-02 22:19:11 +0800

    删除购物车商品业务

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -32,4 +32,11 @@ public interface ShoppingCartService {
     * @return
     * */
     void cleanShoppingCart();
+
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
