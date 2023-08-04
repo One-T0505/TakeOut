69e2b83d8d62950aed38b9176f9bf36b0eb917c3
oneT <15549316580@163.com>
2023-08-04 20:47:21 +0800

    处理定时任务

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * ymy
@@ -16,4 +20,15 @@ public interface OrderMapper {
     * @param orders
     * */
     void insert(Orders orders);
+
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
 }
--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -1,5 +1,7 @@
 package com.sky.task;
 
+import com.sky.constant.MessageConstant;
+import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * ymy
@@ -26,8 +29,41 @@ public class OrderTask {
     * 每分钟执行一次
     * */
     @Scheduled(cron = "0 * * * * ?")
-    public void processTimeoutOrder(){
-        log.info("定时处理超时订单: {}", LocalDateTime.now());
+    public void processTimeoutUnpaidOrder(){
+        log.info("定时处理超时未付款订单: {}", LocalDateTime.now());
 
+        LocalDateTime orderTime = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> expireOrders = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, orderTime);
+
+        if (expireOrders != null && expireOrders.size() > 0){
+            expireOrders.forEach(order -> {
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("订单超时未付款，自动取消");
+                order.setCancelTime(LocalDateTime.now());
+
+                orderMapper.update(order);
+            });
+        }
+    }
+
+    /*
+    * 处理超时派送的订单
+    *
+    **/
+
+    @Scheduled(cron = "0 0 1 * * ?")  // 每天凌晨1点触发一次
+    public void processTimeoutDeliveryOrder(){
+        log.info("定时处理超时派送的订单: {}", LocalDateTime.now());
+
+        LocalDateTime orderTime = LocalDateTime.now().plusHours(-1); // 这个就可以查到上一天的订单
+        List<Orders> expireOrders = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, orderTime);
+
+        if (expireOrders != null && expireOrders.size() > 0){
+            expireOrders.forEach(order -> {
+                order.setStatus(Orders.COMPLETED);
+
+                orderMapper.update(order);
+            });
+        }
     }
 }
